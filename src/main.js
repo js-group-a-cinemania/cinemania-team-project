7551d88cb9f757ed0325e4fc9c10bc78cc638307
keserizzet <izzetfurkankeserwtk@gmail.com>
2025-02-11 01:05:39 +0300

    last update

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import './js/init';
 import axios from 'axios';
 import "./js/library.js";
-import "./js/filters.js";
-import "./js/scrollUp.js";
-import "./js/loader.js";
 import "./js/catalog.js";
