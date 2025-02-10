d434e42e1808cf972713bcde90dd12fc736c416b
your_name <orkunkorcak@gmail.com>
2025-02-11 00:50:54 +0300

    edit main

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import './js/init';
 import axios from 'axios';
-import "./js/library.js";
-import "./js/filters.js";
-import "./js/scrollUp.js";
-import "./js/loader.js";
-import "./js/catalog.js";
+import './js/library.js';
+import './js/filters.js';
+import './js/scrollUp.js';
+import './js/loader.js';
+import './js/catalog.js';
+import {
+  getGenres,
+  fetchTrendingMovies,
+  renderMovies,
+  openModal,
+} from './fileName.js';
+
+renderMovies(3);
+
+document
+  .querySelector('#viewAll')
+  .addEventListener('click', () => renderMovies());
