94956ba72dd146af2cb31ea585bc2bd16fd6ff94
your_name <orkunkorcak@gmail.com>
2025-02-11 18:35:51 +0300

    edit upcoming

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 import "./js/library.js";
 import "./js/catalog.js";
 import "./js/storage.js";
+import './js/upcoming.js';
 import "./js/footer.js";
 import "./js/weekly-trends.js";
-import "./js/upcoming.js";
